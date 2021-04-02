087ff9bbaba545810d965b06ba29d8193595204c
coo.find <coo.find@coofindui-MacBookPro.local>
2021-04-02 13:14:17 +0900

    Fix bug with properties

--- src/main/kotlin/com/github/findcoo/jenkins/plugin/ScdfTaskExecutor.kt
+++ src/main/kotlin/com/github/findcoo/jenkins/plugin/ScdfTaskExecutor.kt
@@ -56,11 +56,11 @@ class ScdfTaskExecutor(private val log: PrintStream? = null) {
         if (properties == null) return mapOf()
         return properties.split(",")
             .fold(mutableMapOf<String, String>()) { map: MutableMap<String, String>, it: String ->
-                val propTuple = it.split("=")
+                val propTuple = it.split("=", 2)
                 if (propTuple.size == 2) {
                     map.set(propTuple[0], propTuple[1])
                 }
-                return map
+                map
             }
     }
 
