3810cf055081b061ae84c224520b7466424e78b9
coo.find <coo.find@coofindui-MacBookPro.local>
2021-04-02 10:05:17 +0900

    Fix remove global configuration

--- src/main/kotlin/com/github/findcoo/jenkins/plugin/ScdfTaskNotifier.kt
+++ src/main/kotlin/com/github/findcoo/jenkins/plugin/ScdfTaskNotifier.kt
@@ -1,41 +1,44 @@
 package com.github.findcoo.jenkins.plugin
 
+import com.cloudbees.plugins.credentials.CredentialsMatchers
 import com.cloudbees.plugins.credentials.CredentialsProvider
+import com.cloudbees.plugins.credentials.common.StandardCredentials
+import com.cloudbees.plugins.credentials.common.StandardListBoxModel
+import com.cloudbees.plugins.credentials.domains.URIRequirementBuilder
 import hudson.Extension
 import hudson.Launcher
 import hudson.model.AbstractBuild
 import hudson.model.AbstractProject
 import hudson.model.BuildListener
+import hudson.model.Item
+import hudson.model.queue.Tasks
+import hudson.security.ACL
 import hudson.tasks.BuildStepDescriptor
 import hudson.tasks.BuildStepMonitor
 import hudson.tasks.Notifier
 import hudson.tasks.Publisher
 import hudson.util.ListBoxModel
+import org.kohsuke.stapler.AncestorInPath
 import org.kohsuke.stapler.DataBoundConstructor
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
+import org.kohsuke.stapler.QueryParameter
+import jenkins.model.Jenkins
+import hudson.model.Queue
 
 
 class ScdfTaskNotifier
 @DataBoundConstructor
 constructor(
   val url: String,
+  var tokenUri: String? = null,
+  var credentialsId: String? = null,
   val taskName: String,
   val properties: String? = null,
   val arguments: String? = null
 ) : Notifier() {
 
-  companion object {
-    private val log: Logger = LoggerFactory.getLogger(ScdfTaskNotifier::class.java)
-  }
-
   override fun perform(build: AbstractBuild<*, *>, launcher: Launcher, listener: BuildListener?): Boolean {
-    val server = ScdfGlobalConfiguration.get().servers.filter { it.url == url }.first()
-    val credentialsId = server.credentialsId
-    val credentials = if (credentialsId != null) {
-      CredentialsProvider.findCredentialById(credentialsId, ScdfCredentials::class.java, build)
-    } else {
-      null
+    val credentials = credentialsId?.let {
+      CredentialsProvider.findCredentialById(it, ScdfCredentials::class.java, build)
     }
 
     ScdfTaskExecutor(listener!!.logger).executeTask(
@@ -43,7 +46,7 @@ constructor(
       credentials?.clientId,
       credentials?.clientSecret,
       credentials?.scope,
-      server.tokenUri,
+      tokenUri,
       taskName,
       properties,
       arguments
@@ -56,20 +59,35 @@ constructor(
   @Extension
   class ScdfTaskNotifierDescriptor : BuildStepDescriptor<Publisher>() {
 
-    companion object {
-      private val log: Logger = LoggerFactory.getLogger(ScdfTaskNotifierDescriptor::class.java)
-    }
-
     override fun getDisplayName(): String = "Execute SCDF task"
 
     override fun isApplicable(jobType: Class<out AbstractProject<*, *>>): Boolean = true
 
-    fun doFillUrlItems(): ListBoxModel {
-      val items = ListBoxModel()
-      ScdfGlobalConfiguration.get().servers.forEach {
-        items.add(it.url)
+    fun doFillCredentialsIdItems(@AncestorInPath item: Item?, @QueryParameter credentialsId: String, @QueryParameter uri: String?): ListBoxModel {
+      val result = StandardListBoxModel()
+      if (item == null) {
+        if (!Jenkins.get().hasPermission(Jenkins.ADMINISTER)) {
+          return result.includeCurrentValue(credentialsId)
+        }
+      } else {
+        if (!item.hasPermission(Item.EXTENDED_READ)
+          && !item.hasPermission(CredentialsProvider.USE_ITEM)
+        ) {
+          return result.includeCurrentValue(credentialsId)
+        }
       }
-      return items
+      return result //
+        .includeEmptyValue() //
+        .includeMatchingAs(
+          if (item is Queue.Task) Tasks.getAuthenticationOf(item as Queue.Task) else ACL.SYSTEM,
+          item,
+          StandardCredentials::class.java,
+          URIRequirementBuilder.fromUri(uri).build(),
+          CredentialsMatchers.anyOf(
+            CredentialsMatchers.instanceOf(ScdfCredentials::class.java)
+          )
+        )
+        .includeCurrentValue(credentialsId)
     }
   }
 }
